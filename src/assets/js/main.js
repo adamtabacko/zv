137f54aa893f8a34fa61a839d5987ce353ae5c1b
Adam Tabacko <adam.tabacko@fores.group>
2021-12-02 12:20:30 +0100

    Init project

---
+++ src/assets/js/main.js
@@ -0,0 +1,73 @@
+
+(function($) {
+	"use strict";
+
+	// -----------------------------------------------------------------------------
+	// This section contain all code for page header component
+	// -----------------------------------------------------------------------------
+	var siteHeader = $( '#navbar' );
+	var scrolling = false,
+		previousTop = 0,
+		currentTop = 0,
+		scrollDelta = 10,
+		scrollOffset = 150;
+
+	if ( siteHeader.length ) {
+		checkSimpleNavigation( currentTop );
+
+		$( window ).on( 'scroll', function() {
+			if ( ! scrolling ) {
+				scrolling = true;
+				( ! window.requestAnimationFrame ) ? setTimeout( autoHideHeader, 250 ) : requestAnimationFrame( autoHideHeader );
+			}
+		} );
+
+		function autoHideHeader() {
+			var currentTop = $( window ).scrollTop();
+
+			checkSimpleNavigation( currentTop );
+
+			previousTop = currentTop;
+			scrolling = false;
+		}
+
+		function checkSimpleNavigation( currentTop ) {
+			if ( currentTop > scrollOffset ) {
+				siteHeader.addClass( 'navbar--collapsed' );
+				return;
+			}
+			siteHeader.removeClass( 'navbar--collapsed' );
+		}
+	}
+
+
+	const swiper_testimonials = new Swiper('.swiper-testimonials', {
+		loop: true,
+		// centeredSlides: true,
+		// slidesPerView: 1,
+		spaceBetween: 50,
+
+		// Navigation arrows
+		grabCursor: true,
+		pagination: {
+			el: ".swiper-pagination",
+		},
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+	});
+
+	const swiper_members = new Swiper('.swiper-members', {
+		loop: true,
+		centeredSlides: true,
+		slidesPerView: "auto",
+		spaceBetween: 30,
+
+		// Navigation arrows
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+	});
+})( jQuery );
